bacc9902c5a1107cd9f21317eef085f12b74b811
playmaker <chaoleicai@gmail.com>
2021-01-10 13:25:50 +0100

    apply penalty when run piocher

--- Uno/PiocherCommand.cpp
+++ Uno/PiocherCommand.cpp
@@ -16,6 +16,13 @@ void PiocherCommand::run()
     gameView->afficher("Vous venez de piocher la carte suivante :");
     gameView->afficher(pioche->toStringLess());
     gameModel->getPlayerManager()->getCurrentPlayer()->getHand()->addData(pioche);
+
+    if(dynamic_cast<UnoGameModel*>(gameModel)->currentPenalty != 0){
+            gameView->afficher("Vous avez pris la penalite");
+            gameView->afficher("Vous allez piocher " + std::to_string(dynamic_cast<UnoGameModel*>(gameModel)->currentPenalty)+ " cartes");
+            dynamic_cast<UnoGameModel*>(gameModel)->applyPenalty();
+    }
+
     gameView->afficher("Voici votre nouvel main: ");
     dynamic_cast<UnoGameView*>(gameView)->afficherPlayersCollection(gameModel->getPlayerManager()->getCurrentPlayer()->getHand());
     *actionEnCours = false;
